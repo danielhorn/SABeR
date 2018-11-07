17cb0ab527bb3b96f3e23ca4352d99fa612c530c
NilsJannik <42466039+NilsJannik@users.noreply.github.com>
2018-11-07 16:03:40 +0100

    auskommentierung vergessen

--- R/ClusterFunctionKMeans.R
+++ R/ClusterFunctionKMeans.R
@@ -4,7 +4,7 @@ clusterFunctionKMeans <- function(clust.data,
   
   # Bestimmt oprimale Clustergroesse durch gapStatistik
   nb <- NbClust::NbClust(as.matrix(clust.data), method = "kmeans", 
-                index = "gap", alphaBeale = 0.1)
+                index = "dunn", alphaBeale = 0.1)
   opt.Clusteranzahl <- nb$Best.nc[1]
   
   kmeans.cluster <- kmeans(clust.data,opt.Clusteranzahl,nstart = 20)
@@ -45,12 +45,12 @@ clusterFunctionKMeans <- function(clust.data,
 # plot(1:k.max, sil, type = "b", pch = 19,frame = FALSE, 
 #      xlab = "Number of clusters k")
 # abline(v = which.max(sil), lty = 2)
-
-
-
-set.seed(123)
-km.res <- kmeans(data, 2, nstart = 25)
-
-# Visualize k-means clusters
-fviz_cluster(km.res, data = data, geom = "point",
-             stand = FALSE, frame.type = "norm")
+# 
+# 
+# 
+# set.seed(123)
+# km.res <- kmeans(data, 2, nstart = 25)
+# 
+# # Visualize k-means clusters
+# fviz_cluster(km.res, data = data, geom = "point",
+#              stand = FALSE, frame.type = "norm")
