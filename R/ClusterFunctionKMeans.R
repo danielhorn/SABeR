1f091f864f538f2c9bfe14c32830700352f13a97
NilsJannik <42466039+NilsJannik@users.noreply.github.com>
2018-11-07 15:54:44 +0100

    temporere Version von ClusterFunctionKMeans. Finale Version muss noch erstellt werden

--- R/ClusterFunctionKMeans.R
+++ R/ClusterFunctionKMeans.R
@@ -16,35 +16,35 @@ clusterFunctionKMeans <- function(clust.data,
 }
 
 
-####
-# Was spuckt alles 2 aus: gap, ch, frey, mcclain, dunn
-# Was spuckt 3 zurueck: duda, pseudot2, ball
-# Was spuckt 4 zurueck: tau
-# Was spuckt 5 zurueck: ratkowskiy
-library(NbClust)
-nb <- NbClust::NbClust(as.matrix(clust.data), method = "kmeans", 
-                       index = "dunn", alphaBeale = 0.1)
-(opt.Clusteranzahl <- nb$Best.nc[1])
-
-####
-library(factoextra)
-fviz_nbclust(clust.data, kmeans, method = "wss")
-###
-
-library(cluster)
-k.max <- 10
-data <- clust.data
-sil <- numeric(k.max)
-for(i in 2:k.max){
-  km.res <- kmeans(data, centers = i, nstart = 25)
-  ss <- silhouette(km.res$cluster, dist(data))
-  sil[i] <- mean(ss[,3])
-}
-
-
-plot(1:k.max, sil, type = "b", pch = 19,frame = FALSE, 
-     xlab = "Number of clusters k")
-abline(v = which.max(sil), lty = 2)
+# ####
+# # Was spuckt alles 2 aus: gap, ch, frey, mcclain, dunn
+# # Was spuckt 3 zurueck: duda, pseudot2, ball
+# # Was spuckt 4 zurueck: tau
+# # Was spuckt 5 zurueck: ratkowskiy
+# library(NbClust)
+# nb <- NbClust::NbClust(as.matrix(clust.data), method = "kmeans", 
+#                        index = "dunn", alphaBeale = 0.1)
+# (opt.Clusteranzahl <- nb$Best.nc[1])
+# 
+# ####
+# library(factoextra)
+# fviz_nbclust(clust.data, kmeans, method = "wss")
+# ###
+# 
+# library(cluster)
+# k.max <- 10
+# data <- clust.data
+# sil <- numeric(k.max)
+# for(i in 2:k.max){
+#   km.res <- kmeans(data, centers = i, nstart = 25)
+#   ss <- silhouette(km.res$cluster, dist(data))
+#   sil[i] <- mean(ss[,3])
+# }
+# 
+# 
+# plot(1:k.max, sil, type = "b", pch = 19,frame = FALSE, 
+#      xlab = "Number of clusters k")
+# abline(v = which.max(sil), lty = 2)
 
 
 
